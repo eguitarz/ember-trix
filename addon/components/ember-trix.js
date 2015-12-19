3a07b1f385f4e230f2a85011d82e3e1c80680d3e
Dale Ma <dalema22@gmail.com>
2015-12-19 00:21:41 -0800

    fix closure actions

--- addon/components/ember-trix.js
+++ addon/components/ember-trix.js
@@ -1,27 +1,28 @@
 import Ember from 'ember';
 import layout from '../templates/components/ember-trix';
 
+const TRIX_EVENTS = [
+  'trix-attachment-add',
+  'trix-attachment-remove',
+  'trix-blur',
+  'trix-change',
+  'trix-file-accept',
+  'trix-focus',
+  'trix-initialize',
+  'trix-selection-change',
+];
+
 export default Ember.Component.extend({
   layout: layout,
   classNames: ['trix-editor-wrapper'],
-  trixEvents: [
-    'trix-attachment-add',
-    'trix-attachment-remove',
-    'trix-blur',
-    'trix-change',
-    'trix-file-accept',
-    'trix-focus',
-    'trix-initialize',
-    'trix-selection-change',
-  ],
 
   didInsertElement() {
     let $trix = this.$('trix-editor');
-    let trixEvents = this.get('trixEvents');
-    trixEvents.forEach(eventName => {
-      if (this.attrs.eventName) {
+    TRIX_EVENTS.forEach(eventName => {
+      if (this.attrs.hasOwnProperty(eventName)) {
         $trix.on(eventName, event => {
-          this.attrs.eventName(event);
+          let { [`${eventName}`] : eventHandler } = this.attrs;
+          eventHandler(event);
         });
       }
     });
