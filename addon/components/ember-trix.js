63144fbeeff6734d9212c1c09efea6788c092563
Dale Ma <dalema22@gmail.com>
2015-12-19 00:03:28 -0800

    support closure actions

--- addon/components/ember-trix.js
+++ addon/components/ember-trix.js
@@ -3,5 +3,27 @@ import layout from '../templates/components/ember-trix';
 
 export default Ember.Component.extend({
   layout: layout,
-  classNames: ['trix-editor-wrapper']
+  classNames: ['trix-editor-wrapper'],
+  trixEvents: [
+    'trix-attachment-add',
+    'trix-attachment-remove',
+    'trix-blur',
+    'trix-change',
+    'trix-file-accept',
+    'trix-focus',
+    'trix-initialize',
+    'trix-selection-change',
+  ],
+
+  didInsertElement() {
+    let $trix = this.$('trix-editor');
+    let trixEvents = this.get('trixEvents');
+    trixEvents.forEach(eventName => {
+      if (this.attrs.eventName) {
+        $trix.on(eventName, event => {
+          this.attrs.eventName(event);
+        });
+      }
+    });
+  }
 });
