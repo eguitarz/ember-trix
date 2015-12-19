fe68fa4a5509339650befbcce7e748d62766aa0a
Dale Ma <dalema22@gmail.com>
2015-12-18 23:28:55 -0800

    dummy now shows ember-trix

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 /* jshint node: true */
 'use strict';
 
+let path = require('path');
+
 module.exports = {
-  name: 'ember-trix'
+  name: 'ember-trix',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(
+      path.join(app.bowerDirectory, 'trix/dist/trix.js'),
+      { type: 'vendor' }
+    );
+
+    app.import(
+      path.join(app.bowerDirectory, 'trix/dist/trix.css'),
+      { type: 'vendor' }
+    );
+  }
 };
